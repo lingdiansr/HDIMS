8bd2eded0376ada510d00eb3ffd545926641be1b
lingdianshiren <2077358365@qq.com>
2023-12-24 13:27:22 +0800

    补完getter setter、toString 构造函数

--- src/Entity/PID.java
+++ src/Entity/PID.java
@@ -5,5 +5,45 @@ public class PID {
     public String PDno;
     public short PDnum;
 
-    // Getters and setters with DAO update calls similar to Admin class
+    public PID() {
+    }
+
+    public PID(int pno, String PDno, short PDnum) {
+        Pno = pno;
+        this.PDno = PDno;
+        this.PDnum = PDnum;
+    }
+
+    @Override
+    public String toString() {
+        return "PID{" +
+                "Pno=" + Pno +
+                ", PDno='" + PDno + '\'' +
+                ", PDnum=" + PDnum +
+                '}';
+    }
+
+    public int getPno() {
+        return Pno;
+    }
+
+    public void setPno(int pno) {
+        Pno = pno;
+    }
+
+    public String getPDno() {
+        return PDno;
+    }
+
+    public void setPDno(String PDno) {
+        this.PDno = PDno;
+    }
+
+    public short getPDnum() {
+        return PDnum;
+    }
+
+    public void setPDnum(short PDnum) {
+        this.PDnum = PDnum;
+    }
 }
--- src/Entity/Supplier.java
+++ src/Entity/Supplier.java
@@ -6,5 +6,55 @@ public class Supplier {
     public String Saddr;
     public String Sphone;
 
-    // Getters and setters with DAO update calls similar to Admin class
+    public Supplier() {
+    }
+
+    public Supplier(String sno, String sname, String saddr, String sphone) {
+        Sno = sno;
+        Sname = sname;
+        Saddr = saddr;
+        Sphone = sphone;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "Sno='" + Sno + '\'' +
+                ", Sname='" + Sname + '\'' +
+                ", Saddr='" + Saddr + '\'' +
+                ", Sphone='" + Sphone + '\'' +
+                '}';
+    }
+
+    public String getSno() {
+        return Sno;
+    }
+
+    public void setSno(String sno) {
+        Sno = sno;
+    }
+
+    public String getSname() {
+        return Sname;
+    }
+
+    public void setSname(String sname) {
+        Sname = sname;
+    }
+
+    public String getSaddr() {
+        return Saddr;
+    }
+
+    public void setSaddr(String saddr) {
+        Saddr = saddr;
+    }
+
+    public String getSphone() {
+        return Sphone;
+    }
+
+    public void setSphone(String sphone) {
+        Sphone = sphone;
+    }
 }
