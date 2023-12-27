dcd47c27924c855d706d7580e9aa014327af6dc4
mystate1224 <2473501617@qq.com>
2023-12-27 19:32:39 +0800

    管理员界面进行了修改
    Signed-off-by: mystate1224 <2473501617@qq.com>

--- src/UI/Warehousekeeper.java
+++ src/UI/Warehousekeeper.java
@@ -8,102 +8,109 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-//任务一：按钮没调整好大小，左右很空
-//任务一：工具条的高度没调整好，上下很空
-//任务三：按钮对应的表格没实现响应
-//任务四：表格边上的滚动还没实现
-public class Warehousekeeper extends JFrame implements ActionListener {
 
+public class Warehousekeeper extends JFrame implements ActionListener {
+    JTabbedPane ButtonPane = new JTabbedPane();//三个工具条共享一个空间
     //供应商工具条
     JToolBar SupplierInformationToolbar = new JToolBar("供应商信息");
-    JButton SupplierNameButton = new JButton("名字");
-    JButton SupplierAddressButton = new JButton("地址");
-    JButton SupplierPhoneButton = new JButton("电话");
+
+    JPanel SupplierInformationPanel = new JPanel();
+    JPanel SP = new JPanel();
+
 
     //药品信息工具条
     JToolBar MedicationInformationToolbar = new JToolBar("药品信息");
-    JButton MedicationNameButton = new JButton("药品名称");
-    JButton MedicationNoButton = new JButton("药品编号");
-    JButton MedicationBatchesButton = new JButton("药品批次");
-    JButton MedicationNumberButton = new JButton("药品数量");
-    JButton MedicationPurchasingstaffButton = new JButton("药品采购人员");
-    JButton MedicationDestructionstaffButton = new JButton("药品销毁人员");
+
+    JPanel MedicationInformationPanel = new JPanel();
 
     //时间信息工具条
     JToolBar TimeInformationToolbar = new JToolBar("时间信息");
-    JButton ShelfLifeButton = new JButton("保质期");
-    JButton InboundTimeButton = new JButton("入库时间");
-    JButton OutboundTimeButton = new JButton("保质期");
-    JButton DestructionTimeButton = new JButton("销毁时间");
-    JTabbedPane ButtonPane = new JTabbedPane();
 
-    //搜索组件
-    JPanel Seacherpannel = new JPanel();
-    JLabel SearcherLable = new JLabel("搜索");
-    JTextField SearcherField = new JTextField();
-    JButton SureButton = new JButton("确定");
+    JPanel TimeInformationPanel = new JPanel();
+    //标签页面
 
-    JTable table = new JTable(10, 10);
-    JScrollPane scrollPane = new JScrollPane(table);
+
+    //搜索组件一
+    JPanel Seacherpannel1 = new JPanel();
+    JPanel Seacherpannel2 = new JPanel();
+    JPanel Seacherpannel3 = new JPanel();
+
+    JLabel SearcherLable1 = new JLabel("搜索");
+    JLabel SearcherLable2 = new JLabel("搜索");
+    JLabel SearcherLable3 = new JLabel("搜索");
+
+    JTextField SearcherField1 = new JTextField();
+    JTextField SearcherField2 = new JTextField();
+    JTextField SearcherField3 = new JTextField();
+    JButton SureButton1 = new JButton("确定");
+    JButton SureButton2 = new JButton("确定");
+    JButton SureButton3 = new JButton("确定");
+
+    JTable table1 = new JTable(10, 10);
+    JTable table2 = new JTable(10, 10);
+    JTable table3 = new JTable(10, 10);
+
 
     public Warehousekeeper() {
         this.setTitle("仓库管理员界面");
         this.setSize(800, 600);
-        //this.setLayout(new BorderLayout());
-        this.setLayout(new GridLayout(4, 1));//第一排是选择按钮，第二排是具体信息按钮，第三排是搜索框，第四排是各种具体信息（很多）
+        this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-
-        //以下是对工具条大小的调整
-//        SupplierInformationToolbar.setLayout(new FlowLayout(FlowLayout.LEFT));
-//        SupplierInformationToolbar.setFloatable(false); // 禁止浮动
-//        //SupplierNameButton.setPreferredSize(new Dimension(50,50));
-//        int buttonHeight = SupplierNameButton.getPreferredSize().height; // 获取按钮的高度
-//        SupplierInformationToolbar.setPreferredSize(new Dimension(100, buttonHeight)); // 设置工具条的大小
-
-//将按钮加入供应商工具条
-        SupplierInformationToolbar.add(SupplierNameButton);
-        SupplierInformationToolbar.add(SupplierAddressButton);
-        SupplierInformationToolbar.add(SupplierPhoneButton);
-
-//将按钮加入药物信息工具条
-        MedicationInformationToolbar.add(MedicationNameButton);
-        MedicationInformationToolbar.add(MedicationNoButton);
-        MedicationInformationToolbar.add(MedicationBatchesButton);
-        MedicationInformationToolbar.add(MedicationNumberButton);
-        MedicationInformationToolbar.add(MedicationPurchasingstaffButton);
-        MedicationInformationToolbar.add(MedicationDestructionstaffButton);
-
-//将按钮加入时间信息工具条
-        TimeInformationToolbar.add(ShelfLifeButton);
-        TimeInformationToolbar.add(InboundTimeButton);
-        TimeInformationToolbar.add(OutboundTimeButton);
-        TimeInformationToolbar.add(DestructionTimeButton);
 
 
-//三个工具条加入标签面板
+        SupplierInformationToolbar.setLayout(new BoxLayout(SupplierInformationToolbar, BoxLayout.Y_AXIS));
+        // 搜索组件
+
+        SearcherField1.setColumns(50);
+        Seacherpannel1.add(SearcherLable1);
+        Seacherpannel1.add(SearcherField1);
+        Seacherpannel1.add(SureButton1);
+        Seacherpannel1.setPreferredSize(new Dimension(300, 100));
+
+        SearcherField2.setColumns(50);
+        Seacherpannel2.add(SearcherLable2);
+        Seacherpannel2.add(SearcherField2);
+        Seacherpannel2.add(SureButton2);
+        Seacherpannel2.setPreferredSize(new Dimension(300, 100));
+
+        SearcherField3.setColumns(50);
+        Seacherpannel3.add(SearcherLable3);
+        Seacherpannel3.add(SearcherField3);
+        Seacherpannel3.add(SureButton3);
+        Seacherpannel3.setPreferredSize(new Dimension(300, 100));
+
+        // 表格实现
+        table1.setPreferredScrollableViewportSize(new Dimension(10, 10));
+        table2.setPreferredScrollableViewportSize(new Dimension(10, 10));
+        table3.setPreferredScrollableViewportSize(new Dimension(10, 10));
+
+        // 供应商界面设置布局方式,搜索在顶层，表格在中间
+        SP.setLayout(new BorderLayout());
+        SP.add(Seacherpannel1, BorderLayout.NORTH);
+        SP.add(new JScrollPane(table1), BorderLayout.CENTER); // 使用JScrollPane将表格包装起来，以实现滚动
+        SupplierInformationToolbar.add(SP);
+
+        MedicationInformationPanel.setLayout(new BorderLayout());
+        MedicationInformationPanel.add(Seacherpannel2, BorderLayout.NORTH);
+        MedicationInformationPanel.add(new JScrollPane(table2), BorderLayout.CENTER); // 使用JScrollPane将表格包装起来，以实现滚动
+        MedicationInformationToolbar.add(MedicationInformationPanel);
+
+        TimeInformationPanel.setLayout(new BorderLayout());
+        TimeInformationPanel.add(Seacherpannel3, BorderLayout.NORTH);
+        TimeInformationPanel.add(new JScrollPane(table3), BorderLayout.CENTER); // 使用JScrollPane将表格包装起来，以实现滚动
+        TimeInformationToolbar.add(TimeInformationPanel);
+
+        // 三个工具条加入标签面板
         ButtonPane.addTab("供应商信息", null, SupplierInformationToolbar);
         ButtonPane.addTab("药品信息", null, MedicationInformationToolbar);
         ButtonPane.addTab("时间信息", null, TimeInformationToolbar);
 
-//搜索组件放入中间容器
-        SearcherField.setColumns(19);
-        Seacherpannel.add(SearcherLable);
-        Seacherpannel.add(SearcherField);
-        Seacherpannel.add(SureButton);
-        Seacherpannel.setPreferredSize(new Dimension(300, 20));
-//表格实现
-
-        table.setPreferredScrollableViewportSize(new Dimension(10, 30));
-        // getContentPane().add(scrollPane,BorderLayout.CENTER);
-//        this.add(ButtonPanel);
-        this.add(ButtonPane);
-        this.add(Seacherpannel);
-        this.add(table);
-
-        SureButton.addActionListener(this);
+        // 设置工具条在顶部
+        this.add(ButtonPane, BorderLayout.CENTER);//没有其他了，直接填满
+        this.setVisible(true);
     }
+
 
     public static void main(String[] s) {
         Warehousekeeper frame = new Warehousekeeper();
@@ -112,14 +119,14 @@ public class Warehousekeeper extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==SureButton && SupplierInformationToolbar.isEnabled())
+        if (e.getSource() == SureButton1 && SupplierInformationToolbar.isEnabled())
         {
-            String searchtext =SearcherField.getText();
+            String searchtext = SearcherField1.getText();
             AdminService as = new AdminService();
             Object[][] searchResult= as.search(searchtext);
             String[] columnNames = {"Sno", "Sname", "Saddr", "Sphone"};
 
-            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            DefaultTableModel model = (DefaultTableModel) table1.getModel();
             model.setDataVector(searchResult, columnNames);
         }
     }
