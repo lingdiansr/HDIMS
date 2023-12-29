926539496ad9f3fc7b3f207283378bd9f58b66bd
lingdianshiren <2077358365@qq.com>
2023-12-29 20:41:09 +0800

    管理员查询药品、运行配置

--- src/Util/InterfaceUtil.java
+++ src/Util/InterfaceUtil.java
@@ -1,7 +1,10 @@
 package Util;
 
+import Entity.Prescription;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.util.Date;
 
 public class InterfaceUtil {
     public static Object[][] convertJTableToObjectArray(JTable table) {
@@ -17,5 +20,29 @@ public class InterfaceUtil {
         }
 
         return data;
+    }
+    public Prescription getSelectedPrescriptionFromTable(JTable table) {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow != -1) { // 确保有选中的行
+            // 通过选中的行索引获取行数据
+            Object[] rowData = new Object[7]; // 假设有7列数据
+            for (int i = 0; i < 7; i++) {
+                rowData[i] = table.getModel().getValueAt(selectedRow, i);
+            }
+
+            // 将行数据封装到 Prescription 对象中
+            Prescription prescription = new Prescription();
+            prescription.Pno = (int) rowData[0];
+            prescription.Pid = (String) rowData[1];
+            prescription.Dno = (String) rowData[2];
+            prescription.Ptime = (Date) rowData[3];
+            prescription.Nno = (String) rowData[4];
+            prescription.Htime = (Date) rowData[5];
+            prescription.Pstate = (boolean) rowData[6];
+
+            return prescription;
+        } else {
+            return null; // 如果没有选中的行，则返回 null
+        }
     }
 }
