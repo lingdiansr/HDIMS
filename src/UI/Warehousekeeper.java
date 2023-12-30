e66c0c6895028f2689b5b1c7f3c9179ce0f4519b
mystate1224 <2473501617@qq.com>
2023-12-30 09:01:32 +0800

    加了几个按钮
    Signed-off-by: mystate1224 <2473501617@qq.com>

--- src/UI/Warehousekeeper.java
+++ src/UI/Warehousekeeper.java
@@ -8,13 +8,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static DAO.SupplierDAO.insertSupplier;
+
 
 public class Warehousekeeper extends JFrame implements ActionListener {
-    JTabbedPane ButtonPane = new JTabbedPane();//三个工具条共享一个空间
+    JTabbedPane ButtonPane = new JTabbedPane();//个工具条共享一个空间
     //供应商工具条
     JToolBar SupplierInformationToolbar = new JToolBar("供应商信息");
+    private DefaultTableModel supplierTableModel;
 
-    JPanel SupplierInformationPanel = new JPanel();
     JPanel SP = new JPanel();
 
 
@@ -23,32 +25,31 @@ public class Warehousekeeper extends JFrame implements ActionListener {
 
     JPanel MedicationInformationPanel = new JPanel();
 
-    //时间信息工具条
-    JToolBar TimeInformationToolbar = new JToolBar("时间信息");
 
-    JPanel TimeInformationPanel = new JPanel();
-    //标签页面
-
-
-    //搜索组件一
+    //搜索组件
     JPanel Seacherpannel1 = new JPanel();
     JPanel Seacherpannel2 = new JPanel();
-    JPanel Seacherpannel3 = new JPanel();
+
 
     JLabel SearcherLable1 = new JLabel("搜索");
     JLabel SearcherLable2 = new JLabel("搜索");
-    JLabel SearcherLable3 = new JLabel("搜索");
+
 
     JTextField SearcherField1 = new JTextField();
     JTextField SearcherField2 = new JTextField();
-    JTextField SearcherField3 = new JTextField();
+
     JButton SureButton1 = new JButton("确定");
     JButton SureButton2 = new JButton("确定");
-    JButton SureButton3 = new JButton("确定");
+    JButton AddButton1 = new JButton("添加");
+    JButton AddButton2 = new JButton("添加");
+    JButton DeleteButton1 = new JButton("删除");
+    JButton DeleteButton2 = new JButton("删除");
+    JButton ResetButton1 = new JButton("修改");
+    JButton ResetButton2 = new JButton("修改");
 
-    JTable table1 = new JTable(25, 4);
-    JTable table2 = new JTable(25, 3);
-    JTable table3 = new JTable(25, 10);
+    //
+    JTable table1 = new JTable(25, 6);
+    JTable table2 = new JTable(25, 6);
 
 
     public Warehousekeeper() {
@@ -59,33 +60,40 @@ public class Warehousekeeper extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         SureButton1.addActionListener(this);
         SureButton2.addActionListener(this);
-        SureButton3.addActionListener(this);
+        AddButton1.addActionListener(this);
+        AddButton2.addActionListener(this);
+        DeleteButton1.addActionListener(this);
+        DeleteButton2.addActionListener(this);
+        ResetButton1.addActionListener(this);
+        ResetButton2.addActionListener(this);
+
 
         SupplierInformationToolbar.setLayout(new BoxLayout(SupplierInformationToolbar, BoxLayout.Y_AXIS));
         // 搜索组件
 
-        SearcherField1.setColumns(50);
+        SearcherField1.setColumns(30);
         Seacherpannel1.add(SearcherLable1);
         Seacherpannel1.add(SearcherField1);
         Seacherpannel1.add(SureButton1);
+        Seacherpannel1.add(AddButton1);
+        Seacherpannel1.add(DeleteButton1);
+        Seacherpannel1.add(ResetButton1);
         Seacherpannel1.setPreferredSize(new Dimension(300, 100));
 
-        SearcherField2.setColumns(50);
+        SearcherField2.setColumns(30);
         Seacherpannel2.add(SearcherLable2);
         Seacherpannel2.add(SearcherField2);
         Seacherpannel2.add(SureButton2);
+        Seacherpannel2.add(AddButton2);
+        Seacherpannel2.add(DeleteButton2);
+        Seacherpannel2.add(ResetButton2);
         Seacherpannel2.setPreferredSize(new Dimension(300, 100));
 
-        SearcherField3.setColumns(50);
-        Seacherpannel3.add(SearcherLable3);
-        Seacherpannel3.add(SearcherField3);
-        Seacherpannel3.add(SureButton3);
-        Seacherpannel3.setPreferredSize(new Dimension(300, 100));
 
         // 表格实现
         table1.setPreferredScrollableViewportSize(new Dimension(10, 10));
         table2.setPreferredScrollableViewportSize(new Dimension(10, 10));
-        table3.setPreferredScrollableViewportSize(new Dimension(10, 10));
+
 
         // 供应商界面设置布局方式,搜索在顶层，表格在中间
         SP.setLayout(new BorderLayout());
@@ -98,15 +106,10 @@ public class Warehousekeeper extends JFrame implements ActionListener {
         MedicationInformationPanel.add(new JScrollPane(table2), BorderLayout.CENTER); // 使用JScrollPane将表格包装起来，以实现滚动
         MedicationInformationToolbar.add(MedicationInformationPanel);
 
-        TimeInformationPanel.setLayout(new BorderLayout());
-        TimeInformationPanel.add(Seacherpannel3, BorderLayout.NORTH);
-        TimeInformationPanel.add(new JScrollPane(table3), BorderLayout.CENTER); // 使用JScrollPane将表格包装起来，以实现滚动
-        TimeInformationToolbar.add(TimeInformationPanel);
 
-        // 三个工具条加入标签面板
+        // 两个工具条加入标签面板
         ButtonPane.addTab("供应商信息", null, SupplierInformationToolbar);
         ButtonPane.addTab("药品信息", null, MedicationInformationToolbar);
-        ButtonPane.addTab("时间信息", null, TimeInformationToolbar);
 
         // 设置工具条在顶部
         this.add(ButtonPane, BorderLayout.CENTER);//没有其他了，直接填满
@@ -121,25 +124,38 @@ public class Warehousekeeper extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == SureButton1 && SupplierInformationToolbar.isEnabled())
-        {
+        if (e.getSource() == SureButton1 && SupplierInformationToolbar.isEnabled()) {
             String searchtext = SearcherField1.getText();
             AdminService as = new AdminService();
-            Object[][] searchResult= as.searchSupplier(searchtext);
+            Object[][] searchResult = as.searchSupplier(searchtext);
             String[] columnNames = {"编号", "供应商名称", "供应商地址", "联系方式"};
             DefaultTableModel model = new DefaultTableModel(searchResult, columnNames);
             model.addRow(new Object[]{});
             table1.setModel(model);
-        } else if (e.getSource()==SureButton2 && MedicationInformationToolbar.isEnabled()) {
+        } else if (e.getSource() == SureButton2 && MedicationInformationToolbar.isEnabled()) {
             System.out.println("t2");
             String searchtext = SearcherField2.getText();
             AdminService as = new AdminService();
-            Object[][] searchResult= as.searchDrug(searchtext);
+            Object[][] searchResult = as.searchDrug(searchtext);
             String[] columnNames = {"编号", "药品名称", "保质期（天）"};
             DefaultTableModel model = new DefaultTableModel(searchResult, columnNames);
             table2.setModel(model);
-        }
+        } else if (e.getSource() == AddButton1 && SupplierInformationToolbar.isEnabled()) {
 
+
+        } else if (e.getSource() == AddButton2) {
+
+        } else if (e.getSource() == DeleteButton1) {
+
+        } else if (e.getSource() == DeleteButton2) {
+
+        } else if (e.getSource() == ResetButton1) {
+
+        } else if (e.getSource() == ResetButton2) {
+
+        }
     }
 }
+
+
 
