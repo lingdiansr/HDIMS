30a11df42a69104f596796b16f4b3be206d8c096
lingdianshiren <2077358365@qq.com>
2024-01-15 10:01:14 +0800

    界面层与逻辑层分离框架示例

---
+++ src/Service/DoctorActionListener.java
@@ -0,0 +1,12 @@
+package Service;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DoctorActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
